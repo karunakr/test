f8541c271f772a5f6ee3a3dfd92c71fb215df582
karun <karun003@gmail.com>
2016-10-26 22:38:36 +0530

    added

--- assets/javascripts/menus.js
+++ assets/javascripts/menus.js
@@ -10,6 +10,7 @@ function enableMenus() {
 	if($('body').hasClass('no-js')){
 	$('body').removeClass('no-js');
 	};
+	
 	//attach a listener to each li that has a child ul, and then slide submenus down or up depending upon mouse position
 	menu.find('li').each(function() {
 		if ($(this).find('ul').length > 0 ) {
